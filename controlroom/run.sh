2d1efc65087eb5e8e47d76d945b13460adad7337
Sam <splavigne@gmail.com>
2018-04-03 17:32:05 -0400

    add run script

---
+++ controlroom/run.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+BASE="/Users/sam/autotv/"
+
+cd $BASE
+source env/bin/activate
+
+case $1 in scrape-homes)
+  echo "Scraping homes"
+  cd homeshow
+  python scrape_and_prep.py
+  ;;
+generate-homes)
+  echo "Generating homes"
+  cd homeshow
+  python compose.py
+  ;;
+generate-cooking)
+  echo "Generating cooking"
+  cd cookingshow2
+  python compose.py
+  ;;
+generate-shopping)
+  echo "Generating shopping"
+  cd shoppingshow
+  python get_baba.py
+  ;;
+*)
+  echo "none"
+  ;;
+esac
